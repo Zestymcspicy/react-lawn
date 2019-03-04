9eb67b8583bff969b048bc48ca78d56734b99c12
Harold Beckerman <halbeckerman@yahoo.com>
2019-03-04 13:43:32 -0600

    set destination point. fetching directions from mapbox-sdk TODO: show directions on mapbox-gl map

--- src/Map.js
+++ src/Map.js
@@ -1,50 +1,67 @@
 import React, { Component } from 'react';
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
-import Directions from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
-import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
-
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
+const MapboxGeocoder = require('@mapbox/mapbox-gl-geocoder')
+const client = require('@mapbox/mapbox-sdk')
+const mbxDirections = require('@mapbox/mapbox-sdk/services/directions')
 
 
 class Map extends Component {
   constructor(props){
     super(props)
     this.state = {
-      aThing : true
+      userLngLat: [],
+      destination: []
     }
+    this.mbxToken = 'pk.eyJ1IjoiemVzdHltY3NwaWN5IiwiYSI6ImNqc281djVneTA5MzAzeXJ2ZWVoMjhmdzMifQ.uT5Hz9PEBvuLwVrZkrkp8A'
+    this.baseClient = client({ accessToken: this.mbxToken});
+    this.directionsService = mbxDirections(this.baseClient);
   }
 
 
 
 componentDidMount (){
-  const myApp = this
-  mapboxgl.accessToken = 'pk.eyJ1IjoiemVzdHltY3NwaWN5IiwiYSI6ImNqc281djVneTA5MzAzeXJ2ZWVoMjhmdzMifQ.uT5Hz9PEBvuLwVrZkrkp8A';
-  this.map = new mapboxgl.Map({
-    container: this.myMap,
-    style: 'mapbox://styles/mapbox/navigation-guidance-day-v2',
-    zoom: 14,
-    boxZoom: true,
-    pitch: 60
-  });
-  this.userLocationMarker= new mapboxgl.Marker()
-  this.map.addControl(new mapboxgl.GeolocateControl({
-    positonOptions: {
-      enableHighAccuracy: true
-    },
-    trackUserLocation: true
-  }));
-  this.directions = new Directions({
-    accessToken: mapboxgl.accessToken,
-    profile: 'mapbox/driving',
-    steps: true,
-    approaches: 'curb'
-  });
-  this.map.addControl(this.directions, 'top-left');
-
+  const myApp = this;
   navigator.geolocation.getCurrentPosition(function (pos) {
     let loc = [pos.coords.longitude, pos.coords.latitude];
+    mapboxgl.accessToken = myApp.mbxToken;
+    myApp.map = new mapboxgl.Map({
+      container: myApp.myMap,
+      style: 'mapbox://styles/mapbox/navigation-guidance-day-v2',
+      zoom: 14,
+      boxZoom: true,
+      pitch: 60
+    });
+    myApp.userLocationMarker= new mapboxgl.Marker()
+    myApp.destinationGeocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      proximity: loc,
+    })
+    myApp.destinationGeocoder.on('result', function(ev) {
+      myApp.parseResult(ev)
+      });
+    myApp.map.addControl(myApp.destinationGeocoder, 'top-left')
+
+    // myApp.directions = new Directions({
+    //   accessToken: mapboxgl.accessToken,
+    //   profile: 'mapbox/driving',
+    //   steps: true,
+    //   approaches: ';curb',
+    //   geocoder: {proximity: loc, reverseGeocode: true, },
+    //   controls: {profileSwitcher: false},
+    //   bannerInstructions: true
+    // });
+    // myApp.map.addControl(myApp.directions, 'top-left');
+    myApp.setState({userLngLat: loc})
+
     myApp.setMapCenter(loc);
     myApp.setUserLocation(loc);
   })
+}
+
+parseResult(obj) {
+  let loc = obj.result.geometry.coordinates;
+  this.setState({destination: loc}, this.setDirections)
 }
 
 setMapCenter(location)  {
@@ -53,9 +70,35 @@ setMapCenter(location)  {
 
 setUserLocation(location) {
     this.userLocationMarker.setLngLat(location);
-    this.userLocationMarker.addTo(this.map)
+    this.userLocationMarker.addTo(this.map);
 }
 
+setDestination(location){
+  this.destinationLocationMarker = new mapboxgl.Marker();
+  this.destinationLocationMarker.setLngLat(location);
+  this.destinationLocationMarker.addTo(this.map);
+}
+
+setDirections() {
+  this.setDestination(this.state.destination);
+
+  this.directionsService.getDirections({
+    profile: 'driving',
+    waypoints: [
+      {
+        coordinates: this.state.userLngLat
+      },
+      {
+        coordinates: this.state.destination,
+        approach: 'curb'
+      }
+  ]
+  })
+  .send()
+  .then(response => {
+      console.log(response.body)
+  })
+}
 
 render(){
   const mapStyle = {
