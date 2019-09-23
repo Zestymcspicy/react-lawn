43edd3755f77c8e48f946c720c05cc010c07179a
Zestymcspicy <halbeckerman@yahoo.com>
2019-09-23 15:05:25 -0500

    test function

--- src/TurnByTurn.js
+++ src/TurnByTurn.js
@@ -1,4 +1,4 @@
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+// import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
 
 const TurnByTurn = {
   directions: {},
@@ -8,46 +8,8 @@ const TurnByTurn = {
   layersArray: [],
 
 
-  route: [
-    {
-      longitude:-95.238279,
-      latitude:38.96607,
-      heading: 90
-      },
-    {
-      longitude:-95.236688,
-      latitude:38.973047,
-      heading:270
-    },
-    {
-      longitude:-95.2355,
-      latitude:38.976597,
-      heading:180
-    },
-    {
-      longitude:-95.231233,
-      latitude:38.976365,
-      heading:0
-    }
-  ],
-//maneuver.instructions
-  readDirections: function() {
 
-    let legs = this.directions.legs
-    let steps = legs.map(x => x.steps)
-    let instructions = steps[0].map(x=> x.maneuver.instruction)
-    // console.log(legs)
-    // console.log(steps)
-    // console.log(instructions)
-  },
 
-  follow: function(event) {
-    console.log(event)
-    this.map.easeTo({
-      center:[event.coords.longitude, event.coords.latitude],
-      bearing: event.coords.heading,
-    })
-  },
 
   addLine: function() {
     TurnByTurn.routeLayer = this.map.getLayer(this.presentLayerId);
@@ -76,7 +38,10 @@ const TurnByTurn = {
       }
     })
   },
+  startNavigation: function() {
+    console.log("starting nav");
 
+  }
 }
 
 export { TurnByTurn }
